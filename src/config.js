0ced72e683834fb222afaa84bda7c6ced86fe38d
Anna Perevedentseva <anna.pereved@gmail.com>
2023-04-20 20:55:02 +0200

    feat: add project bundle

---
+++ src/config.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+const assert_1 = __importDefault(require("assert"));
+dotenv_1.default.config();
+const { PORT, HOST, HOST_URL, API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, } = process.env;
+(0, assert_1.default)(PORT, 'PORT is required');
+(0, assert_1.default)(HOST, 'HOST is required');
+exports.config = {
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    firebaseConfig: {
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID,
+    },
+};
